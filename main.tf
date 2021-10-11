d075d63d5e59a2cb393f2d8efd0f6b734e6475c9
Emmanuel289 <emmirald@gmail.com>
2021-10-11 14:04:37 -0600

    Updated configuration of subnets

--- main.tf
+++ main.tf
@@ -1,28 +1,33 @@
 
+# Define VPC configuration
 resource "aws_vpc" "prod_web" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    "Name" : "test-vpc"
+    "Name" : "rackspace-vpc"
   }
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.prod_web.id
-  cidr_block = "10.0.1.0/24"
+# Define configuration for public subnet
+resource "aws_subnet" "az1" {
+  vpc_id            = aws_vpc.prod_web.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ca-central-1a"
 
   tags = {
-    "Name" : "subnet"
+    "Name" : "rackspace-public"
   }
 }
 
-resource "aws_subnet" "subnet2" {
+# Define configuration for private subnet
+resource "aws_subnet" "az2" {
 
-  vpc_id     = aws_vpc.prod_web.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id            = aws_vpc.prod_web.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ca-central-1b"
 
   tags = {
-    "Name" : "subnet"
+    "Name" : "rackspace-private"
   }
 
 }
