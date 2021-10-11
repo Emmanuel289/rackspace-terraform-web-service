333699fb4e868d30972e46a8e8bb449a3422fd27
Emmanuel289 <emmirald@gmail.com>
2021-10-11 12:42:37 -0600

    Configuration for Terraform AWS Provider

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region                  = "ca-central-1"
+  profile                 = "rackspace"
+}
+
+
