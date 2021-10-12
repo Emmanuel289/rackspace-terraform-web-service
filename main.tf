3b1cf1f07b5c0f544795194774ffed360ea2ff26
Emmanuel289 <emmirald@gmail.com>
2021-10-12 15:02:50 -0600

    Autoscaling policies and security groups

--- main.tf
+++ main.tf
@@ -1,40 +1,4 @@
-
-# # Define VPC configuration
-# resource "aws_vpc" "prod_web" {
-#   cidr_block = "10.0.0.0/16"
-
-#   tags = {
-#     "Name" : "rackspace-vpc"
-#   }
-# }
-
-
-# # Define configuration for public subnet
-# resource "aws_subnet" "az1" {
-#   vpc_id            = aws_vpc.prod_web.id
-#   cidr_block        = "10.0.0.0/24"
-#   availability_zone = "ca-central-1a"
-
-#   tags = {
-#     "Name" : "rackspace-public"
-#   }
-# }
-
-# # Define configuration for private subnet
-# resource "aws_subnet" "az2" {
-
-#   vpc_id            = aws_vpc.prod_web.id
-#   cidr_block        = "10.0.1.0/24"
-#   availability_zone = "ca-central-1b"
-
-#   tags = {
-#     "Name" : "rackspace-private"
-#   }
-
-# }
-
-
-# Terraform module which creates VPC resources on AWS
+# Registry module which creates AWS VPC resources
 # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
 
 module "vpc" {
@@ -56,11 +20,153 @@ module "vpc" {
   }
 }
 
+# EC2 key-pair
+# resource "aws_key_pair" "dev_web" {
+#   key_name   = "${var.name}-key-pair"
+#   public_key = "${file("~/.ssh/id_ed25519.pub")}"
+# }
+
+
+
+# AutoScaling group for EC2 instances
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group
+
+resource "aws_autoscaling_group" "dev_web" {
+  # availability_zones  = var.availability_zones
+  name = "${var.name}-asg"
+  max_size                  = 3
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  vpc_zone_identifier = module.vpc.private_subnets
+
+  # launch templates are to be preferred over launch configurations to ease versioning and access to
+  # latest updates and features
+  # https://docs.aws.amazon.com/autoscaling/ec2/userguide/LaunchTemplates.html
+
+  launch_template {
+    id      = aws_launch_template.dev_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Rackspace"
+    value               = "true"
+    propagate_at_launch = "true"
+  }
+}
+
+# AutoScaling policy for scaling up
+resource "aws_autoscaling_policy" "scale_up" {
+    name = "${var.name}-scaleup"
+    autoscaling_group_name = aws_autoscaling_group.dev_web.id
+    adjustment_type = "ChangeInCapacity"
+    scaling_adjustment = 1
+    cooldown = 60
+    policy_type = "SimpleScaling"
+
+}
+
+# AutoScaling policy for scaling down
+resource "aws_autoscaling_policy" "scale_down" {
+    name = "${var.name}-scaledown"
+    autoscaling_group_name = aws_autoscaling_group.dev_web.id
+    adjustment_type = "ChangeInCapacity"
+    scaling_adjustment = -1
+    cooldown = 60
+    policy_type = "SimpleScaling"
+
+}
+
+# Launch template for creating autoscaling group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template
+
+resource "aws_launch_template" "dev_web" {
+  name_prefix   = "dev-web"
+  description   = "Launch t2.micro instances in the subnets of the VPC"
+  image_id      = var.ami
+  instance_type = "t2.micro"
+  # key_name = aws_key_pair.dev_web.key_name
+
+  tags = {
+    Rackspace   = "true"
+    Environment = "dev"
+  }
+
+}
+
+# Load balancer attachment to connect load balancer to autoscaling group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_attachment
+
+resource "aws_autoscaling_attachment" "dev_web" {
+  autoscaling_group_name = aws_autoscaling_group.dev_web.id
+  elb                    = aws_elb.dev_web.id
+}
+
+# Elastic load balancer for registering web server instances
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elb
+resource "aws_elb" "dev_web" {
+  name               = "rackspace-elb"
+  # availability_zones = var.availability_zones
+  subnets         = module.vpc.private_subnets
+  security_groups = [aws_security_group.lb_sg.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 10
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing = true
+
+
+
+}
+
+
+# Security group for EC2 instances
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow inbound http traffic only from load balancer"
+  vpc_id      = module.vpc.vpc_id
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Rackspace   = "true"
+    Environment = "dev"
+  }
+
+}
 
 # Security group for load balancer
 
-resource "aws_security_group" "dev_web" {
-  name        = "dev-web"
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
   description = "Allow inbound http traffic to load balancer from anywhere"
   vpc_id      = module.vpc.vpc_id
 
@@ -86,67 +192,11 @@ resource "aws_security_group" "dev_web" {
 
 }
 
-# Launch template for creating autoscaling group
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template
-
-resource "aws_launch_template" "dev_web" {
-  name_prefix   = "dev-web"
-  description   = "Launch t2.micro instances in the subnets of the VPC"
-  image_id      = "ami-02e136e904f3da870"
-  instance_type = "t2.micro"
-
-  tags = {
-    Rackspace   = "true"
-    Environment = "dev"
-  }
-
-}
-
-# AutoScaling group for EC2 instances
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group
-
-resource "aws_autoscaling_group" "dev_web" {
-  # availability_zones  = var.availability_zones
-  vpc_zone_identifier = module.vpc.private_subnets
-  desired_capacity    = 2
-  max_size            = 3
-  min_size            = 1
-
-  launch_template {
-    id      = aws_launch_template.dev_web.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Rackspace"
-    value               = "true"
-    propagate_at_launch = "true"
-  }
-}
-
-# Load balancer attachment to connect load balancer to autoscaling group
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_attachment
-
-resource "aws_autoscaling_attachment" "dev_web" {
-  autoscaling_group_name = aws_autoscaling_group.dev_web.id
-  elb                    = aws_elb.dev_web.id
-}
-
-# Elastic load balancer for registering web server instances
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elb
-resource "aws_elb" "dev_web" {
-  name               = "rackspace-elb"
-  # availability_zones = var.availability_zones
-  subnets            = module.vpc.private_subnets
-  security_groups    = [aws_security_group.dev_web.id]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
 
 
-}
+
+
+
+
+
 
