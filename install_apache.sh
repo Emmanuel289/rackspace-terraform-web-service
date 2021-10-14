af881655d12e77576a27cc6a869b30b547e4adc0
Emmanuel289 <emmirald@gmail.com>
2021-10-14 06:53:00 -0600

    Modify scripting

--- install_apache.sh
+++ install_apache.sh
@@ -16,5 +16,11 @@ sudo chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} +
 find /var/www -type f -exec sudo chmod 0664 {} +
 
+# Install stress tool to test scaling policy
+sudo amazon-linux-extras install epel -y
+sudo yum install stress -y
+# Spawn 8 workers spinning on sqrt() with a timeout of 60 seconds
+sudo stress --cpu 8 -v --timeout 60s
+
 # Create an index.html file with a welcome message and hostname at /var/www/html/ path
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
