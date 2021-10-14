0b08754dff2745bf5aad57e48e18622df6724995
Emmanuel289 <emmirald@gmail.com>
2021-10-14 05:08:07 -0600

    Added more variable declarations for resources

--- variables.tf
+++ variables.tf
@@ -1,11 +1,4 @@
-variable "cidr-block" {
-  description = "VPC CIDR Block"
-  type        = string
-
-  default = "10.0.0.0/16"
-
-}
-
+# Variable definitions for resources
 variable "name" {
   description = "Name of Project"
   type        = string
@@ -14,47 +7,53 @@ variable "name" {
 
 }
 
-variable "vpc_azs" {
-  description = "Availability zones for VPC"
+variable "cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+
+  default = "10.0.0.0/16"
+
+}
+
+variable "azs" {
+  description = "Availability zones for VPC's subnets"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
 }
 
 variable "private_subnets" {
-  description = "A list of private subnets inside the VPC"
+  description = "Private subnets within VPC"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
 }
 
 variable "public_subnets" {
-  description = "A list of public subnets inside the VPC"
+  description = "Public subnets within VPC"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24" ]
 
 }
-
-variable "ami" {
-  description = "AMI used for creating EC2 instances"
-  type        = string
-  default     = "ami-02e136e904f3da870"
-
-
-
-}
-
-
-variable "key" {
-    type        = string
-    description = "ssh key."
-}
-
 
 variable "tags" {
-  description = "Tags to apply to resources in project"
+  description = "Tags to apply to resources within project"
   type        = map(string)
   default = {
     Rackspace   = "true"
     Environment = "dev"
+    propagate_at_launch = true
   }
+}
+
+variable "metrics" {
+
+  description = "A list of metrics to collect."
+  type = list(string)
+  default = [
+    "GroupDesiredCapacity", "GroupInServiceCapacity", "GroupPendingCapacity",
+    "GroupMinSize", "GroupMaxSize", "GroupInServiceInstances", "GroupPendingInstances",
+    "GroupStandbyInstances", "GroupStandbyCapacity", "GroupTerminatingCapacity",
+    "GroupTerminatingInstances", "GroupTotalCapacity", "GroupTotalInstances"
+  ]
+
 }
